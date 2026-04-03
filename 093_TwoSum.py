28e6216d1c92c587aa7a9b034bc163924c6dd4f8
PrakyathReddy <prakyathreddyk@gmail.com>
2026-04-03 07:08:05 +0530

    reducing O(n)

--- 093_TwoSum.py
+++ 093_TwoSum.py
@@ -1,10 +1,21 @@
-def TwoSum(numbers):
+# def TwoSum(numbers):
+#     list = []
+#     for i in numbers:
+#         for j in numbers:
+#             if (i+j) == 9:
+#                 list.append(numbers.index(i))
+#     return list
+
+# nums = [2, 7, 11, 15]
+# print(TwoSum(nums))
+
+def TwoSum(numbers, target):
     list = []
     for i in numbers:
-        for j in numbers:
-            if (i+j) == 9:
-                list.append(numbers.index(i))
+        if i<target and (target - i) in numbers:
+            list.append(numbers.index(i))
     return list
 
 nums = [2, 7, 11, 15]
-print(TwoSum(nums))
+target = 9
+print(TwoSum(nums, target))
